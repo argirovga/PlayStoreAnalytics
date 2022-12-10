9a5aa4978756ea5c903b8ddd6f2ea9fb237d62b2
argirovga <argirovgosha@gmail.com>
2022-12-10 16:20:12 +0300

    Started front building

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import pandas as pd
+import plotly.express as px
+df = pd.read_csv("/Users/goshaargirov/Downloads/archive/googleplaystore.csv")
+df.dropna(how='any', inplace=True)
+
 
 
 def view_main_page(request):
-
-    return render(request, 'main.html')
+    df["Price"] = df["Price"].str.replace('$', '').astype(float)
+    mean_rating = round(df.Rating.mean(), 2)
+    fig = px.scatter(x=range(10), y=range(10))
+    fig.write_html('/Users/goshaargirov/Documents/PlayStoreAnalytics/templates/plot.html', full_html=False)
+    payload = {
+        'mean_rating' : str(mean_rating)
+    }
+    return render(request, 'index.html', payload)
